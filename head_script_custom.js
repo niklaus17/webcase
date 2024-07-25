bbd95acdb68d4ebc79029324019f2d6955ab0540
Nicolae N <35431660+niklaus17@users.noreply.github.com>
2024-07-26 01:00:36 +0300

    Update head_script_custom.js

--- head_script_custom.js
+++ head_script_custom.js
@@ -1,67 +1,79 @@
-// Adăugarea stilurilor CSS
-var css = `
-  body.no-scroll {
-    overflow: hidden;
-  }
-  .global-confirm-checkbox-block label {
-    pointer-events: none;
-  }
-  .my-footer {
-    margin-top: 100px;
-    margin-left: 28px;
-    display: flex;
-    gap: 26px;
-  }
-  .my-footer a {
-    font-family: "Inter";
-    font-style: normal;
-    font-weight: 500;
-    font-size: 14px;
-    line-height: 150%;
-    letter-spacing: 0.01em;
-    color: #0d0e10;
-    text-decoration: none !important;
-  }
-  @media only screen and (max-width: 600px) {
-    .my-footer {
-      flex-direction: column !important;
-      margin-top: 70px !important;
+// Funcția pentru adăugarea stilurilor CSS
+function addStyles() {
+  var css = `
+    body.no-scroll {
+      overflow: hidden;
     }
-  }
-  .myLoader {
-    position: absolute;
-    left: 0px;
-    display: flex;
-    top: 0px;
-    width: 100vw;
-    height: 100vh;
-    z-index: 9999;
-    align-items: center;
-    justify-content: center;
-    background: white;
-    opacity: 1;
-    transition: opacity 0.5s ease-in-out;
-  }
-`;
+    .global-confirm-checkbox-block label {
+      pointer-events: none;
+    }
+    .my-footer {
+      margin-top: 100px;
+      margin-left: 28px;
+      display: flex;
+      gap: 26px;
+    }
+    .my-footer a {
+      font-family: "Inter";
+      font-style: normal;
+      font-weight: 500;
+      font-size: 14px;
+      line-height: 150%;
+      letter-spacing: 0.01em;
+      color: #0d0e10;
+      text-decoration: none !important;
+    }
+    @media only screen and (max-width: 600px) {
+      .my-footer {
+        flex-direction: column !important;
+        margin-top: 70px !important;
+      }
+    }
+    .myLoader {
+      position: absolute;
+      left: 0px;
+      display: flex;
+      top: 0px;
+      width: 100vw;
+      height: 100vh;
+      z-index: 9999;
+      align-items: center;
+      justify-content: center;
+      background: white;
+      opacity: 1;
+      transition: opacity 0.5s ease-in-out;
+    }
+  `;
 
-// Adăugarea stilurilor CSS în head
-var style = document.createElement('style');
-style.type = 'text/css';
-if (style.styleSheet) {
-  style.styleSheet.cssText = css;
-} else {
-  style.appendChild(document.createTextNode(css));
+  var style = document.createElement('style');
+  style.type = 'text/css';
+  if (style.styleSheet) {
+    style.styleSheet.cssText = css;
+  } else {
+    style.appendChild(document.createTextNode(css));
+  }
+  document.head.appendChild(style);
 }
-document.head.appendChild(style);
 
-// Adăugarea HTML-ului pentru loader
-var loaderDiv = document.createElement('div');
-loaderDiv.className = 'myLoader';
-loaderDiv.innerHTML = '<div id="lottie-container" style="width: 80px; height: 80px"></div>';
-document.body.appendChild(loaderDiv);
+// Funcția pentru adăugarea HTML-ului pentru loader
+function addLoader() {
+  var loaderDiv = document.createElement('div');
+  loaderDiv.className = 'myLoader';
+  loaderDiv.innerHTML = '<div id="lottie-container" style="width: 80px; height: 80px"></div>';
+  document.body.appendChild(loaderDiv);
+}
 
-// Adăugarea scriptului MaskPhone
-var scriptMaskPhone = document.createElement('script');
-scriptMaskPhone.src = 'https://fs.getcourse.ru/fileservice/file/download/a/521488/sc/346/h/19fa9e3b98d0f2a315d2f79f8f3a5ada.js';
-scriptMaskPhone.defer = true;
-document.head.appendChild(scriptMaskPhone);
+// Funcția pentru adăugarea scriptului MaskPhone
+function addMaskPhoneScript() {
+  var scriptMaskPhone = document.createElement('script');
+  scriptMaskPhone.src = 'https://fs.getcourse.ru/fileservice/file/download/a/521488/sc/346/h/19fa9e3b98d0f2a315d2f79f8f3a5ada.js';
+  scriptMaskPhone.defer = true;
+  document.head.appendChild(scriptMaskPhone);
+}
+
+// Așteaptă până când documentul este complet încărcat
+document.addEventListener('DOMContentLoaded', function () {
+  addStyles();
+  addLoader();
+  addMaskPhoneScript();
+});
